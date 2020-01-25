2e47bbe321f0bf667b5377f0442abda5a69552a8
vtranduc <vtranduc@ryerson.ca>
2020-01-25 04:22:28 -0500

    laid out rough structure of the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route } from "react-router-dom";
+import store from "./store";
+import NavBar from "./view/components/NavBar";
+import Home from "./view/pages/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    console.log("Starting the entire app...");
+    console.log(
+      "Here, the app should start making api call to check for use regsitration etc."
+    );
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <NavBar />
+        <BrowserRouter>
+          <Route path="/" exact component={Home} />
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
-
-export default App;
---
+++ src/view/components/NavBar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class NavBar extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div>This is the nav bar here</div>;
+  }
+}
